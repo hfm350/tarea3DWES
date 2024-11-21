5d9f055303a85a0e679ba3bb06394dbae47135a4
hfm350 <naw02083@educastur.es>
2024-11-21 14:13:05 +0100

    Primer commit

---
+++ tarea3dweshfm350/src/main/java/com/hfm350/tarea3dweshfm350/Principal.java
@@ -0,0 +1,21 @@
+package com.hfm350.tarea3dweshfm350;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+public class Principal implements CommandLineRunner{
+
+//	@Autowired
+	
+	
+	
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("INI");
+		System.out.println("-------------");
+		System.out.println("FIN");
+		
+		
+	}
+
+}
